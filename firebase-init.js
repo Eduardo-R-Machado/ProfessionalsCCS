d67d977a599975b9d5d820a75e7f68a7e53000c9
Eduardo Machado <eduardormachado20@gmail.com>
2022-12-19 16:29:34 -0300

    Conectando firebase ao app

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNFTJnUZ78pzT0MuH_rRn1MREIaW5oo84",
+    authDomain: "cadastro-profissionals.firebaseapp.com",
+    projectId: "cadastro-profissionals",
+    storageBucket: "cadastro-profissionals.appspot.com",
+    messagingSenderId: "111730628517",
+    appId: "1:111730628517:web:00cc5d8c5f9fa6a9cc7a4c"
+  };
+
+firebase.initializeApp(firebaseConfig);
